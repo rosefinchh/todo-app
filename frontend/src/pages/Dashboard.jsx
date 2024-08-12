5a9d1c4e53534ede92304ae8b67fbe7bed68630e
rosefinchh <nelsonritz55@gmail.com>
2024-08-12 12:27:39 +0530

    moved all todos functionality to Dashboard; new page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,11 @@
+import Todo from "../components/Todo";
+
 function Dashboard() {
-  return <div>Dashboard page</div>;
+  return (
+    <div>
+      <Todo />
+    </div>
+  );
 }
 
 export default Dashboard;
